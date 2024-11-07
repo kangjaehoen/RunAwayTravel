5187086fe8fbdfb016865faa22f768edba93484c
KangJaeHeon <gjsdms1244@gmail.com>
2024-11-07 19:06:28 +0900

    feat : ReviewRepostiory 테스트 파일 생성

---
+++ src/main/java/com/example/runawaytravel/repository/ReviewRepostiory.java
@@ -0,0 +1,8 @@
+package com.example.runawaytravel.repository;
+
+import com.example.runawaytravel.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepostiory extends JpaRepository<Review,Integer> {
+
+}
