bff376285eb72f79fd27a61e762589ddadb15892
joonho <junho9661@gmail.com>
2024-11-24 21:24:36 +0900

    너무 많은 일이 있었어

--- src/main/java/com/example/runawaytravel/controller/SellCalendar.java
+++ src/main/java/com/example/runawaytravel/controller/SellCalendar.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,8 +20,8 @@ public class SellCalendar {
     @Autowired
     ReservationRepository rr;
     @GetMapping(value = "/resmonth")
-    public ResponseEntity<Map<String,Object>>resmonth(@RequestParam LocalDate start, @RequestParam LocalDate end){
-        String username = "testID";
+    public ResponseEntity<Map<String,Object>>resmonth(@RequestParam LocalDate start, @RequestParam LocalDate end, Principal principal){
+        String username = principal.getName();
         Map<String ,Object> response = new HashMap<>();
         response.put("resmonth", rr.resMonth(username, start,end));
         return new ResponseEntity<>(response, HttpStatus.OK);
